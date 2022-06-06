028ed4d6b15d1a1fe89426049d8dc60fcb425387
Robert Soriano <sorianorobertc@gmail.com>
2022-06-05 23:45:08 -0700

    refactor shims

--- shim.js
+++ shim.js
@@ -1,11 +1,13 @@
 import 'react-native-get-random-values'
 import '@ethersproject/shims'
 
-if (typeof BigInt === 'undefined')
+if (typeof BigInt === 'undefined') {
   global.BigInt = require('big-integer')
+}
 
-if (typeof Buffer === 'undefined')
+if (typeof Buffer === 'undefined') {
   global.Buffer = require('buffer').Buffer
+}
 
 global.btoa = global.btoa || require('base-64').encode
 global.atob = global.atob || require('base-64').decode
